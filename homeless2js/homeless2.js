70dd878b80988784c7182c6531a6f9d2fd8f3911
Rasulwebs <boboqulovrasul80@gamil.com>
2022-08-16 16:58:42 +0500

    JavaScript_masala2

---
+++ homeless2js/homeless2.js
@@ -0,0 +1,468 @@
+"user strict"
+
+//  Classwork
+
+// alert("Ro'yhatdan o'ting !");
+
+// let userName=prompt("Ismingizni kiriting :");
+// let userLogin=prompt("Login kititing :");
+// let userPasword=prompt("Paro; kiriting :");
+// alert("Muvofaqiyatli ro'yhatdan o'tdingiz !");
+
+// let login=prompt("Loginni kiriting");
+// let password=prompt("Parolni kirirting")
+
+// let notif=`${userName} Saytga xush kelibsiz !`
+// let error=`Login ${login} va Parol ${password} xato bo'lishi mumkin !`
+
+// if (userPasword===password && userLogin===login){
+//     document.querySelector("#text").textContent=notif;
+
+// }else{
+//     document.querySelector("#text").textContent=error;
+// }
+
+
+
+
+
+
+
+
+// ======== 1 masala  ==========
+
+// let hafta=+prompt("hafta");
+
+// switch(hafta){
+//     case(1):
+//     alert("Dushanba")
+//     break;
+
+//     case(2):
+//     alert("Seshanba")
+//     break;
+
+//     case(3):
+//     alert("Chorshanba")
+//     break;
+
+//     case(4):
+//     alert("Payshanba")
+//     break;
+
+//     case(5):
+//     alert("Juma")
+//     break;
+
+//     case(6):
+//     alert("Shanba")
+//     break;
+
+//     case(7):
+//     alert("Yakshanba")
+//     break;
+
+//     default:
+//         alert("topilmadi !")
+// }
+
+
+//  ========= 2 masala ========
+
+// let baho=+prompt("baho");
+
+// switch(baho){
+//     case(1):
+//     alert("Yomon")
+//     break;
+
+//     case(2):
+//     alert("Qoniqarsiz")
+//     break;
+
+//     case(3):
+//     alert("Qoniqarli")
+//     break;
+
+//     case(4):
+//     alert("Yaxshi")
+//     break;
+
+//     case(5):
+//     alert("A'lo")
+//     break;
+
+//     default:
+//     alert("Tupoy")
+//     break;
+
+// }
+
+// ====== 3. masala ==========
+
+// let savol = +prompt("oy raqamini tanlab faslni aniqlaymiz");
+
+// switch (savol) {
+//     case 1:
+//         console.log("Qish fasli");
+//         break;
+//     case 2:
+//         console.log("Qish fasli");
+//         break;
+//     case 3:
+//         console.log("Bahor fasli");
+//         break;
+//     case 4:
+//         console.log("Bahor fasli");
+//         break;
+//     case 5:
+//         console.log("Bahor fasli");
+//         break;
+//     case 6:
+//         console.log("Yoz fasli");
+//         break;
+//     case 7:
+//         console.log("Yoz fasli");
+//         break;
+//     case 8:
+//         console.log("Yoz fasli");
+//         break;
+//     case 9:
+//         console.log("Kuz fasli");
+//         break;
+//     case 10:
+//         console.log("Kuz fasli");
+//         break;
+//     case 11:
+//         console.log("Kuz fasli");
+//         break;
+//     case 2:
+//         console.log("Qish fasli");
+//         break;
+//     default:
+//         console.log("Xato")
+
+// }
+
+//  ========  4.masla ==========
+
+// let savol = +prompt("oy raqamini tanlng !");
+
+// switch (savol) {
+//     case 1:
+//         console.log("Qish fasli  Yanvar 31 kun bor");
+//         break;
+//     case 2:
+//         console.log("Qish fasli Fevral 28 kun bor");
+//         break;
+//     case 3:
+//         console.log("Bahor fasli Mart 31 kun bor");
+//         break;
+//     case 4:
+//         console.log("Bahor fasli Aprel 30 kun bor");
+//         break;
+//     case 5:
+//         console.log("Bahor fasli May 31 kun bor");
+//         break;
+//     case 6:
+//         console.log("Yoz fasli Iyun 30 kun bor");
+//         break;
+//     case 7:
+//         console.log("Yoz fasli Iyul 31 kun bor");
+//         break;
+//     case 8:
+//         console.log("Yoz fasli Avgust 31 kun bor");
+//         break;
+//     case 9:
+//         console.log("Kuz fasli Santyabr 30 kun bor");
+//         break;
+//     case 10:
+//         console.log("Kuz fasli Oktyabr 31 kun bor");
+//         break;
+//     case 11:
+//         console.log("Kuz fasli Noyabr 30 kun bor");
+//         break;
+//     case 2:
+//         console.log("Qish fasli Dekabr 31 kun bor");
+//         break;
+//     default:
+//         console.log("Xato")
+
+// }
+
+
+
+
+
+//  ======5. masala ==========
+
+// let c = prompt('amalni tanlang');
+
+// let a = +prompt("sonni kiriting");
+// let b = +prompt(" sonni kiriting");
+
+// switch (c) {
+//     case "-":
+//         console.log(a - b);
+//         break;
+//     case "+":
+//         console.log(a + b);
+//         break;
+
+//     case "*":
+//         console.log(a * b);
+//         break;
+//     case "/":
+//         console.log(a / b);
+//         break;
+// }
+
+
+//   ============ 6. masala ===========
+
+
+// let baho=+prompt("baho");
+
+// switch(baho){
+//     case(1):
+//     alert("Yaxshi o'qisang olasan")
+//     break;
+
+//     case(2):
+//     alert("100 ming so'm")
+//     break;
+
+//     case(3):
+//     alert("350 ming so'm")
+//     break;
+
+//     case(4):
+//     alert("600 ming so'm")
+//     break;
+
+//     case(5):
+//     alert("800 ming so'm")
+//     break;
+
+//     default:
+//     alert("Tupoy")
+//     break;
+
+// }
+
+
+//  ========= 7. masala ==========
+
+// let n =+prompt('n sonni tanlang');
+// let k=+prompt(" k sonni tanlang");
+
+// for(let i=1; i<=n; i++){
+//     console.log(k);
+// }
+
+// ====== 13. masala ==========
+
+// let x=+prompt(" sonni kiriting !");
+
+// for(let i=1; i<=x; i++){
+//     if(x%i==0){
+//         console.log(i);
+//     }else{
+//         console.log("murakkab emas");
+//     }
+    
+// }
+
+// ====== 17.masala ========
+
+// let n=+prompt("sonni kirirting");
+
+// for(let i=1; i<=n; i+=2){
+//     console.log(i);
+
+// }
+
+//  18. masala =========
+
+// let n=+prompt("sonni kirirting");
+
+// for(let i=2; i<=n; i+=2){
+//     console.log(i);
+
+// }
+
+// =========20. masala ==========
+
+//  boshqacharoq usuli: console.log(i*i);
+
+// let n=+prompt("sonni kiriting");
+
+// for(let i=1; i<=n; i++){
+    
+//     console.log(i**2);
+  
+// }
+
+// ====== 22.masala =========
+
+// let n=+prompt("sonni kiriting");
+
+// for(let i=1; i<=n; i++){
+//     i%5==0 ? console.log() : console.log(i);
+// }
+
+//  ====== 23. masala =========
+
+
+
+
+
+
+
+    // ========= 12.masala ========
+
+    // let c=0;
+
+    // let a=+prompt("a sonni kiriting");
+    // let b=+prompt(" b sonni kirirting");
+
+    // for(let i=a; i<=b; i++){
+    //     c+=i;
+
+    // }
+    // console.log(c);
+
+
+// ======14.masla ==========
+
+
+// let a=+prompt(" a  sonni kiriting");
+// let b=+prompt(" b sonni kiriting");
+
+// let c =0;
+
+// for(let i=a; i<=b; i++){
+//     let kv=Math.pow(i,2)
+//     c+=kv;
+// }
+// console.log(c);
+
+
+// =======20.masala==========
+
+// let n=+prompt("n sonni kirirting");
+
+// for(let i=0; i<=n; i++){
+//     let k=n-i;
+//     console.log(k);
+// }
+
+
+// ======== 27.masala =========
+
+// let n=+prompt("n sonni kiriting");
+// let x=+prompt(" x sonni kiriting");
+
+// for(let i=0; i<=n; i++){
+//   if(i%x===0){
+//     console.log(i);
+
+//   }else{
+//     console.log(i+" bo'linmaydi");
+//   }
+
+    
+// }
+
+
+
+
+
+
+
+
+// ======== 10. masala =========
+
+// for(let i=1; i<=10; i++){
+//     console.log(i+"kg " +i * 1000+" so'm ");
+  
+// }
+
+//========= 11.masala ===========
+
+// for(let i=1; i<=10; i++){
+//     console.log(i/10+"kg " +i * 1000+" so'm ");
+  
+// }
+
+
+//  ========= 12. masala  ========
+
+
+
+
+
+
+
+
+// ======== 15. masala ===========
+
+// let ten = 100;
+// let numbers = 0;
+
+// for ( let i = 1; i <= ten; i++){
+//     let n = 1 / i;
+//     numbers = numbers + n;
+//     // +=numbers
+
+// }
+
+// console.log(numbers);
+
+// ===== 21. masala ===========
+
+
+
+// let n =10;
+// let x=2;
+
+
+// for(let i=1; i <=n; i++){
+//     i % x == 0 ? console.log(i) : console.log("bo'linmaydi")
+// }
+
+
+
+//  Qoshimcha masala =============
+
+
+
+// let p=+prompt("Qancha puling bor ?");
+
+// let sb=500*10650;
+// let mt=250*10650;
+// let muz=120*11650;
+
+// let h=sb+mt+muz;
+
+// if(p>h){
+//     document.getElementById('text').textContent="Oq yo'l !"
+    
+
+// }else{
+//     document.getElementById('text').textContent="Yana ozgina kerak !"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
